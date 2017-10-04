6ee313b009f8ca1da24d7c3873c451bfe070312b
Adam Harrison <ad@mharrison.us>
2017-10-04 16:19:04 -0400

    Better Gson support

--- src/main/java/com/ulfric/plugin/data/DataPlugin.java
+++ src/main/java/com/ulfric/plugin/data/DataPlugin.java
@@ -3,6 +3,7 @@ package com.ulfric.plugin.data;
 import com.ulfric.dragoon.activemq.ActiveContainer;
 import com.ulfric.dragoon.conf4j.Conf4jContainer;
 import com.ulfric.dragoon.curator.CuratorContainer;
+import com.ulfric.dragoon.gson.GsonContainer;
 import com.ulfric.dragoon.rethink.RethinkContainer;
 import com.ulfric.dragoon.vault.VaultContainer;
 import com.ulfric.plugin.Plugin;
@@ -10,6 +11,7 @@ import com.ulfric.plugin.Plugin;
 public class DataPlugin extends Plugin {
 
 	public DataPlugin() {
+		install(GsonContainer.class);
 		install(Conf4jContainer.class);
 		install(VaultContainer.class);
 		install(ActiveContainer.class);
