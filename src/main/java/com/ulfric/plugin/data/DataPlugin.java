11db4b3ad7090d65c98d7934cb6e5b63156efde9
Adam Harrison <ad@mharrison.us>
2017-11-25 06:43:23 -0500

    AcroDB support

--- src/main/java/com/ulfric/plugin/data/DataPlugin.java
+++ src/main/java/com/ulfric/plugin/data/DataPlugin.java
@@ -1,11 +1,8 @@
 package com.ulfric.plugin.data;
 
-import com.ulfric.dragoon.activemq.ActivemqContainer;
+import com.ulfric.dragoon.acrodb.AcrodbContainer;
 import com.ulfric.dragoon.conf4j.Conf4jContainer;
-import com.ulfric.dragoon.curator.CuratorContainer;
 import com.ulfric.dragoon.gson.GsonContainer;
-import com.ulfric.dragoon.rethink.RethinkContainer;
-import com.ulfric.dragoon.vault.VaultContainer;
 import com.ulfric.plugin.Plugin;
 
 public class DataPlugin extends Plugin {
@@ -13,10 +10,7 @@ public class DataPlugin extends Plugin {
 	public DataPlugin() {
 		install(GsonContainer.class);
 		install(Conf4jContainer.class);
-		install(VaultContainer.class);
-		install(ActivemqContainer.class);
-		install(RethinkContainer.class);
-		install(CuratorContainer.class);
+		install(AcrodbContainer.class);
 	}
 
 }
